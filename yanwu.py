6e1f8972199286d4f57fffe2f4f024f74f55e80b
Jungleshi <57282160+chengtz@users.noreply.github.com>
2020-03-07 12:21:54 +0800

    Update yanwu.py

--- yanwu.py
+++ yanwu.py
@@ -1,21 +1,39 @@
+  GNU nano 2.5.3                                         File: publisher.py                                                                                         
+
 #!/usr/bin/env python
-# coding:utf-8
+# BEGIN ALL
+# BEGIN SHEBANG
+#!/usr/bin/env python
+# END SHEBANG
+ 
+# BEGIN IMPORT
+import rospy
 import RPi.GPIO as GPIO
 import time
-
+# END IMPORT
+ 
+# BEGIN STD_MSGS
+from std_msgs.msg import Int16
+# END STD_MSGS
 CHANNEL=36
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(CHANNEL,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(CHANNEL,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) 
+yanwu=GPIO.input(CHANNEL)
+#GPIO.input(CHANNEL)
+ 
+rospy.init_node('sense')
+ 
+# BEGIN PUB
+pub = rospy.Publisher('yanwu', Int16)
+# END PUB
+ 
+# BEGIN LOOP
+rate = rospy.Rate(10)
+ 
+#count = 0
+while not rospy.is_shutdown():
+    pub.publish(yanwu)
+    rate.sleep()
+# END LOOP
+# END ALL
 
-
-try:
-         while True:
-          status=GPIO.input(CHANNEL)
-          #print(status)
-          if status == True:
-                        print ('OK')
-          else:
-                        print ('DANGER')
-          time.sleep(0.1)
-except KeyboardInterrupt:
-            GPIO.cleanup()
